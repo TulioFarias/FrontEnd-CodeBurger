a4c4a77f0a5e9b8906015484a65bd98ba70ab58b
TulioFarias <tuliott10@gmail.com>
2023-09-25 14:13:24 -0300

    upgrade api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiCodeBurger = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: 'https://apicodeburger.cyclic.app'
 })
 
 apiCodeBurger.interceptors.request.use(async config => {
